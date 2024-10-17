5ac24d78dbf7970325607bf0cace7de218a9b09c
Ishtiyaq Husain <91125089+ishtiyaq-td@users.noreply.github.com>
2024-10-17 16:57:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+@app.route('/ping')
+def ping():
+    return "Pong!"
+    
 if __name__ == '__main__':
     # Run the app on host '0.0.0.0' and port 5000
     app.run(host='0.0.0.0', port=5000, debug=True)
